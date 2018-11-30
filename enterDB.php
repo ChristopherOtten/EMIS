a460f565e5606cce3f9bf1da23cf3d9c1298f1e1
Christopher Otten <31423098+ChristopherOtten@users.noreply.github.com>
2018-11-30 10:24:34 -0600

    ;

--- enterDB.php
+++ enterDB.php
@@ -43,7 +43,7 @@ VALUES ('$first', '$last', '$usrName', '$pPass', '$patient', '$ID')";
 
 $sql2 = "INSERT INTO patients ". "(first_name, middle_name, last_name, Gender, weight, height, age, blood_pressure, diagnosis, medications, Doctor)". "
 VALUE ('$first', '$middle', '$last', '$gender', '$weight', '$height', '$age', '$bloodP', '$diag', '$meds', '$doctor')";
-echo $sql
+echo $sql;
 
 $sql3 = "INSERT INTO patientInfo ". "(first_name, middle_name, last_name, Gender, address, city, state, zip, cellphone, insurance_provider, insurance_number)". "
 VALUE ('$first', '$middle', '$last', '$gender', '$street', '$city', '$state', '$zip', '$cell', '$providerIns', '$numberIns')";
