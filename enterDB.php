008904dc3ea09e68f2afa27fc1008e130cf2890a
Christopher Otten <31423098+ChristopherOtten@users.noreply.github.com>
2018-12-02 11:26:22 -0600

    Fix query bug

--- enterDB.php
+++ enterDB.php
@@ -70,7 +70,7 @@ if ($row['email'] == $usrName){
 
 //set up sql statement
 $sql = "INSERT INTO generalUsers ". "(firstName, lastName, email, password, permissions, accountID, specialization)". "
-VALUES ('$first', '$last', '$usrName', '$pPass', '$patient', '$ID', '$specialization)";
+VALUES ('$first', '$last', '$usrName', '$pPass', '$patient', '$ID', '$specialization')";
 
 $sql2 = "INSERT INTO patients ". "(first_name, middle_name, last_name, Gender, weight, height, age, blood_pressure, diagnosis, medications, Doctor)". "
 VALUE ('$first', '$middle', '$last', '$gender', '$weight', '$height', '$age', '$bloodP', '$diag', '$meds', '$doctor')";
