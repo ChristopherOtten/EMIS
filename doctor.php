a6767f15193b71b6b6221e429aad84859f78aebf
Christopher Otten <31423098+ChristopherOtten@users.noreply.github.com>
2018-12-02 12:01:20 -0600

    Temp doctor.php

---
+++ doctor.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include("inc_connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>EMIS - Personal Information</title>
+	<link rel="stylesheet" href="patient.css"/>
+	<meta charset="UTF-8">
+	<meta name="description" content="Medical records and Appointment scheduling">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<!-- Main Body-->
+<body>
+	<td>Go sixers </td>
+</body>
+
+</html>
