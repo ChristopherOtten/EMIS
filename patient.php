05851a94713320768778bd52f8adcf299c4c0eea
max-elixir <maximilianguzman@gmail.com>
2018-12-03 11:12:23 -0600

    Test to pull user data and display

--- patient.php
+++ patient.php
@@ -21,7 +21,7 @@ session_start();
     //make array out of query results
     $row = mysqli_fetch_array($query_result);
 
-    echo ("$query_result");
+    echo ("$row");
     //sql to grab info from database 'dateTime'
     //$sql = "SELECT first_name, middle_name, last_name FROM patients" ;
     //$sql .= " WHERE Doctor LIKE 'Star'";
