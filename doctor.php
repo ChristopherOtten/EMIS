fabe74207720924f713d6a4e7394638101421f83
Christopher Otten <31423098+ChristopherOtten@users.noreply.github.com>
2018-12-04 15:42:24 -0600

    New dropdown for doctor

--- doctor.php
+++ doctor.php
@@ -47,59 +47,7 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.dropbtn {
-    background-color: #4CAF50;
-    color: white;
-    padding: 16px;
-    font-size: 16px;
-    border: none;
-    cursor: pointer;
-}
 
-.dropbtn:hover, .dropbtn:focus {
-    background-color: #3e8e41;
-}
-
-#myInput {
-    border-box: box-sizing;
-    background-image: url('searchicon.png');
-    background-position: 14px 12px;
-    background-repeat: no-repeat;
-    font-size: 16px;
-    padding: 14px 20px 12px 45px;
-    border: none;
-    border-bottom: 1px solid #ddd;
-}
-
-#myInput:focus {outline: 3px solid #ddd;}
-
-.dropdown {
-    position: relative;
-    display: inline-block;
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f6f6f6;
-    min-width: 230px;
-    overflow: auto;
-    border: 1px solid #ddd;
-    z-index: 1;
-}
-
-.dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
-
-.dropdown a:hover {background-color: #ddd;}
-
-.show {display: block;}
-</style>
 </head>
 
 <!-- Main Body-->
@@ -182,9 +130,8 @@ Click on the button to open the dropdown menu.<br>
 Use the input field to search for a specific dropdown link.<br>
 
 <div class="dropdown">
-<button onclick="myFunction()" class="dropbtn">Dropdown</button>
-  <div id="myDropdown" class="dropdown-content">
-    <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+
+  <select id="mySelect" onchange="myFunction2()">
 	<?php
 		
 			while($row = mysqli_fetch_assoc($query_result2)){
@@ -194,24 +141,25 @@ Use the input field to search for a specific dropdown link.<br>
 			foreach($items2 as $item){
 			
 				?>
-				<a href="#" id="mylink" onclick="myFunction2(); return false"><?php echo $item["first_name"] . " " . $item["last_name"]?></a>
-				</tr>
+				<option value="<?php echo $item["first_name"] . " " . $item["last_name"]?>"><?php echo $item["first_name"] . " " . $item["last_name"]?>
 				<?php
 			}
 			?>
-  </div>
-</div>
+			</select>
+ </div>
 <br><br><br>
-<div class="reaction">
+
+
 <p id="demo"></p>
-</div>
+
 
 <script>
 function myFunction2() {
-    var x = <?php echo $item["first_name"]?>";
+    var x = document.getElementById("mySelect").value;
     document.getElementById("demo").innerHTML = "You selected: " + x;
 }
 </script>
+
 
 
 <script>
@@ -219,21 +167,6 @@ function myFunction2() {
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
-    }
 }
 </script>
 </body>
