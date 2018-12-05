c9763707a0ec2154314ab82421bee31aba87abf3
Christopher Otten <31423098+ChristopherOtten@users.noreply.github.com>
2018-12-04 23:52:18 -0600

    Add files via upload

--- doctor.php
+++ doctor.php
@@ -163,7 +163,7 @@ function myFunction2() {
     var x = document.getElementById("mySelect").value;
 	<?php
 	$queryJS = "SELECT first_name, last_name, weight, age, blood_pressure, diagnosis, medications FROM patients";
-	$queryJS .= " WHERE " . "Doctor" . " LIKE '" . $row['lastName'] . "' AND last_name LIKE '" . x . "' ";
+	$queryJS .= " WHERE " . "Doctor" . " LIKE '" . $row['lastName'] . "' AND last_name LIKE 'Berg'";
 	$query_resultJS = mysqli_query($MYSQLI,$queryJS)
 	or die ("Invalid query: ".mysqli_error($MYSQLI));
 	$rowJS = mysqli_fetch_array($query_resultJS);
