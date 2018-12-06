99b79dca47814326de462e28633e35404b43de3c
max-elixir <maximilianguzman@gmail.com>
2018-12-05 18:54:04 -0600

    Update patient edit

--- patientUpdateDB.php
+++ patientUpdateDB.php
@@ -33,8 +33,8 @@ $old = mysqli_fetch_array($query_result);
 $oldName = $old['name'];
 
 // Update variables
-$sql2 = "Update patientInfo SET first_name= '".$firstName."', middle_name= '".$middleName."', last_name= '".$lastName."', Gender= '".$gender."', street= '".$street."', city= '".$city."', state= '".$state."', zip= '".$zip."', phone= '".$phone."' WHERE first_name= '".$oldName."' ";
-$sql = "UPDATE generalUsers SET firstName= '".$firstName."' lastName= '".$lastName."' WHERE email=".$_SESSION['email'];
+$sql2 = "Update patientInfo SET first_name= '".$firstName."', middle_name= '".$middleName."', last_name= '".$lastName."', Gender= '".$gender."', street= '".$street."', city= '".$city."', state= '".$state."', zip= '".$zip."', phone= '".$phone."' WHERE first_name='".$oldName."' ";
+$sql = "UPDATE generalUsers SET firstName= '".$firstName."' lastName= '".$lastName."' WHERE email='".$_SESSION['email']."' ";
 
 //plug into database, if it breaks, print error
 if ((mysqli_query($MYSQLI, $sql)) && (mysqli_query($MYSQLI, $sql2))) {
